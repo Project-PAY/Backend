5ad64cfae1077ff53bd4e4cbec4059271ebf345a
unknown <leaf_cis@naver.com>
2019-09-12 23:43:27 +0900

    Edit some code

--- router/auth.ts
+++ router/auth.ts
@@ -2,17 +2,44 @@ import Bell from '@hapi/bell';
 
 const url = "/auth";
 
-const auth = async (server) => {
+interface payload {
+  id : string,
+  password : string,
+}
 
-  await server.register(Bell);
-
-  
+const auth = (server) => {
 
   server.route({
     method: ['POST'],
     path: `${url}/login`,
+    handler: function(request, h) {
+
+      const payload : payload = request.payload;
+
+      if(payload["id"] === undefined || payload["password"]) {
+      }
+
+    },
+    options: {
+        response: {
+          messange : '로그인'
+        }
+    }
+  })
+
+  server.route({
+    method: ['POST'],
+    path: `${url}/signup`,
     handler(request, h) {
-      return '로그인'
+      return '회원가입'
+    }
+  })
+
+  server.route({
+    method: ['POST'],
+    path: `${url}/refresh`,
+    handler(request, h) {
+      return '리프레시 토큰'
     }
   })
 }
